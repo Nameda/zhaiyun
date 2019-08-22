d3f3851c9b879fde3a9ce9224c40480ab054bd4c
wangting04 <wangting04@cd.tuan800.com>
2019-08-22 15:53:06 +0800

    首页

--- pages/home/index.js
+++ pages/home/index.js
@@ -7,6 +7,31 @@ Page({
     imgUrls:[
       "http://img.zcool.cn/community/01281b5c1c7967a80121df904b0c3e.png",
       "http://t-1.tuzhan.com/03c215caab18/c-2/l/2013/04/26/16/c0fd074931cf4be6990fab3803cdac7c.jpg"
+    ],
+    listTab:['推荐','餐饮','房地产','酒店','服装','汽车','美容','游戏'],
+    currentTab:0,
+    lists:[
+      {
+        id:1,
+        pic:'../../static/image/home/pic.png',
+        name:'魏晨阳1',
+        industry:'汽车',
+        resources:'拥有资源：汽车，机油，汽车美容，玻璃,汽车，机油，汽车美容，玻璃汽车，机油，汽车美容，玻璃 '
+      },
+      {
+        id:2,
+        pic:'../../static/image/home/pic.png',
+        name:'魏晨阳2',
+        industry:'汽车',
+        resources:'拥有资源：汽车，机油，汽车美容，玻璃...... '
+      },
+      {
+        id:3,
+        pic:'../../static/image/home/pic.png',
+        name:'魏晨阳3',
+        industry:'汽车',
+        resources:'拥有资源：汽车，机油，汽车美容，玻璃...... '
+      },
     ]
   },
   //事件处理函数
@@ -16,39 +41,17 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  //切换 tab
+  changeType:function(e) {
+    let index =  e.currentTarget.dataset['index'];
+    //判断是否请求数据
+    if(index != this.data.currentTab){
+      this.setData({
+        currentTab :index
+      })
+      //请求数据。。。
+    }
   }
 })
