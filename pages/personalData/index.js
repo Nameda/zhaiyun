c511a83b3827ae91c6ca024f25ec76305a39b94b
wangting04 <wangting04@cd.tuan800.com>
2019-08-23 18:49:33 +0800

    wt

--- pages/personalData/index.js
+++ pages/personalData/index.js
@@ -1,8 +1,26 @@
 Page({
     data:{
+        sex:1,
+        cansee:0,
+        array: ['互联网/IT/电子/通信', '广告/传媒/文化/体育', '金融', '教育培训'],
+        industry:''
     },
     onLoad:function(options){
         // 生命周期函数--监听页面加载
     },
-    
+    changeSex:function(e){
+        this.setData({
+            sex:e.currentTarget.dataset.type
+        })
+    },
+    changePriv:function(e){
+        this.setData({
+            cansee:e.currentTarget.dataset.type
+        })
+    },
+    bindPickerChange: function(e) {
+        this.setData({
+            industry: e.detail.value
+        })
+    },
 })
