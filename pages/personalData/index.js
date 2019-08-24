d6de1501b6ef2e6716ce16ae400f2d553ad8c6ab
nameda <wang_ting0311@163.com>
2019-08-24 12:42:34 +0800

    个人信息

--- pages/personalData/index.js
+++ pages/personalData/index.js
@@ -1,9 +1,25 @@
 Page({
     data:{
-        sex:1,
-        cansee:0,
-        array: ['互联网/IT/电子/通信', '广告/传媒/文化/体育', '金融', '教育培训'],
-        industry:''
+        sex:1, //性别
+        cansee:0, //个人隐私
+        industryList:[
+            '互联网/IT/电子/通信', 
+            '广告/传媒/文化/体育',
+             '金融', 
+             '教育培训',
+             '制药/医疗',
+             '交通/物流/贸易/零售',
+             '专业服务',
+             '房地产/建筑',
+             '汽车',
+             '机械/制造',
+             '消费品',
+             '服务业',
+             '能源/环保',
+             '政府/非盈利机构/其他'
+            ],
+        isShowDialog:false,
+        canSave:false  //保存按钮
     },
     onLoad:function(options){
         // 生命周期函数--监听页面加载
@@ -18,9 +34,18 @@ Page({
             cansee:e.currentTarget.dataset.type
         })
     },
-    bindPickerChange: function(e) {
+    //显示行业选择弹窗
+    showDialog: function() {
         this.setData({
-            industry: e.detail.value
+            isShowDialog: true
         })
     },
+    //选择行业
+    chooseIndustry:function(e){
+        let index = e.currentTarget.dataset.index;
+        this.setData({
+            industry:this.data.industryList[index],
+            isShowDialog: false
+        })
+    }
 })
