4ccbbb14b9a3f9bbfcc173cad5c811c8d0e31e89
wangting04 <wangting04@cd.tuan800.com>
2019-08-27 17:40:41 +0800

    会员

--- pages/joinMember/index.js
+++ pages/joinMember/index.js
@@ -1,7 +1,18 @@
 Page({
     data:{
+        memberType:0,
+        money:0,
+        canPay:false
     },
     onLoad:function(options){
         // 生命周期函数--监听页面加载
+    },
+    //切换会员种类
+    changeType(e){
+        this.setData({
+            memberType:e.currentTarget.dataset.type,
+            money:e.currentTarget.dataset.money,
+            canPay:true
+        })
     }
 })
